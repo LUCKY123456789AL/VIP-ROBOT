fa820baad2e445da01f6dbeb5b5d9240eccd810f
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-10-30 14:59:44 +0530

    Update zombies.py

--- FallenRobot/modules/zombies.py
+++ FallenRobot/modules/zombies.py
@@ -101,7 +101,7 @@ __help__ = """
 *Remove Deleted Accounts*
  ❍ /zombies *:* Starts searching for deleted accounts in the group.
  ❍ /zombies clean *:* Removes the deleted accounts from the group.
-⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊 @THE_VIP_BOY⛦⃕͜🇮🇳 𓊉 𓆩🦋𓆪
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
 
 
