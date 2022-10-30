d8a87d464322addf4c2b663b504ff2448385afd4
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-10-30 15:44:50 +0530

    Update tools.py

--- FallenRobot/modules/tools.py
+++ FallenRobot/modules/tools.py
@@ -1,10 +1,11 @@
 # © @AnonymousBoy1025
 
-__mod_name__ = "Tᴏᴏʟs"
+__mod_name__ = "⚙️𝐓𝐎𝐎𝐋𝐒🛠️"
 
 __help__ = """
 
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
