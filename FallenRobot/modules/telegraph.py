ecf8c24c97e0a31d1f07669ad3b4cf5afce3ee5f
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-10-30 16:04:25 +0530

    Update telegraph.py

--- FallenRobot/modules/telegraph.py
+++ FallenRobot/modules/telegraph.py
@@ -91,6 +91,7 @@ I can upload files to Telegraph
  ❍ /tgm :Get Telegraph Link Of Replied Media
  ❍ /tgt :Get Telegraph Link of Replied Text
  ❍ /tgt [custom name]: Get telegraph link of replied text with custom name.
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
 
-__mod_name__ = "T-Gʀᴀᴘʜ"
+__mod_name__ = "🕺𝐓-𝐆𝐑𝐀𝐏𝐇💃"
