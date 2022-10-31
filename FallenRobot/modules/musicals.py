003009dca4facf5ea74d745fbbed3b7b581d2641
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-10-31 23:45:46 +0530

    Update musicals.py

--- FallenRobot/modules/musicals.py
+++ FallenRobot/modules/musicals.py
@@ -1,6 +1,7 @@
-__mod_name__ = "Mᴜsɪᴄ"
+__mod_name__ = "🦢𝐌𝐔𝐒𝐈𝐂💋"
 
 __help__ = """
  ❍ /song <song name>*:* Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
