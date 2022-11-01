f0ca9339c85259dc18e4aa8e0ca4c64c50d7612e
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-02 02:48:41 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -221,11 +221,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
-            )
-            update.effective_message.reply_text(
-                PM_START_TEXT,
+            update.effective_message.reply_text(PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
