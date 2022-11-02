41a163dbf3004d9b3143957429ff3983012f7ae7
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-02 07:41:47 +0530

    Update group.py

--- FallenRobot/modules/group.py
+++ FallenRobot/modules/group.py
@@ -4,6 +4,7 @@ __help__ = """
  ❍ /delgpic*:* Same as above but to remove group profile pic.
  ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
  ❍ /setdescription <description>*:* Sets new chat description in group.
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
 
-__mod_name__ = "Gʀᴏᴜᴘ"
+__mod_name__ = "🦜𝐆𝐑𝐎𝐔𝐏🦜"
