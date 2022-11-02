28b0f20deffcfc1400c3e58cb707f8fc7f4dd2c7
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-02 14:29:46 +0530

    Update global_bans.py

--- FallenRobot/modules/global_bans.py
+++ FallenRobot/modules/global_bans.py
@@ -421,7 +421,7 @@ def check_and_ban(update, user_id, should_message=True):
             text = (
                 f"<b>Alert</b>: this user is globally banned.\n"
                 f"<code>*bans them from here*</code>.\n"
-                f"<b>Appeal chat</b>: @{SUPPORT_CHAT}\n"
+                f"<b>Appeal chat</b>: @VIP_DUNIA\n"
                 f"<b>User ID</b>: <code>{user_id}</code>"
             )
             user = sql.get_gbanned_user(user_id)
@@ -531,7 +531,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "Aɴᴛɪ-Sᴘᴀᴍ​"
+__mod_name__ = ""🔥𝐀𝐍𝐓𝐈-𝐒𝐏𝐀𝐌🛑"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
