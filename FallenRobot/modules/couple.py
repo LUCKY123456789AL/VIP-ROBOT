ebbefdc34fc7ddeea84ae7293911df1b887cabd9
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-04 11:15:58 +0530

    Update couple.py

--- FallenRobot/modules/couple.py
+++ FallenRobot/modules/couple.py
@@ -76,6 +76,7 @@ __help__ = """
 Choose couples in your chat
 
  ❍ /couple *:* Choose 2 users and send their name as couples in your chat.
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
 """
 
-__mod_name__ = "Cᴏᴜᴘʟᴇ​"
+__mod_name__ = "👑𝐂𝐎𝐔𝐏𝐋𝐄👸​"
