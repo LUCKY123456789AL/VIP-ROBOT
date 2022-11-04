ea16c955fbc0ba4e3c4900f77aa3d31d77173a73
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-04 10:59:08 +0530

    Update dbcleanup.py

--- FallenRobot/modules/dbcleanup.py
+++ FallenRobot/modules/dbcleanup.py
@@ -147,5 +147,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*")
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "DB Cleanup"
+__mod_name__ = "🤤𝐂𝐋𝐄𝐀𝐍𝐔𝐏😎"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
