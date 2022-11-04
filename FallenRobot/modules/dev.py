899a7f1513a09399a3357741eaaa1aa3716031a6
AnonymousBoyxD <113531350+KingXdOp@users.noreply.github.com>
2022-11-04 10:53:05 +0530

    Update dev.py

--- FallenRobot/modules/dev.py
+++ FallenRobot/modules/dev.py
@@ -91,5 +91,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "Dev"
+__mod_name__ = "🐴𝐃𝐄𝐕🐸"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
